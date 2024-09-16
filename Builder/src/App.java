b814892650b600d362edca25b42087d1727dc1ef
Jorge Mario Lasso Ortiz <jorgelasso_1992@hotmail.com>
2024-09-16 15:03:52 -0500

    Adding new Hot Wheels Types to Director

--- Builder/src/App.java
+++ Builder/src/App.java
@@ -5,8 +5,40 @@ public class App {
     public static void main(String[] args) throws Exception {
         Director director = new Director();
 
-        HotWheelsBuilder builder = new HotWheelsBuilder();
-        director.buildAcceleCharged(builder);
-        System.out.println(builder.build());
+        HotWheelsBuilder acceleCharge = new HotWheelsBuilder();
+        director.buildAcceleCharged(acceleCharge);
+        acceleCharge
+                .id(1)
+                .seriesName("AcceleRacers")
+                .model("Rip Rod")
+                .color("red");
+        System.out.println(acceleCharge.build());
+
+        HotWheelsBuilder treasureHunt = new HotWheelsBuilder();
+        director.buildTreasureHunt(treasureHunt);
+        treasureHunt
+                .id(2)
+                .seriesName("Treasure Hunt")
+                .model("Bone Shaker")
+                .color("black");
+        System.out.println(treasureHunt.build());
+
+        HotWheelsBuilder zamac = new HotWheelsBuilder();
+        director.buildZamac(zamac);
+        zamac
+                .id(3)
+                .seriesName("Zamac")
+                .model("Twin Mill")
+                .color("silver");
+        System.out.println(zamac.build());
+
+        HotWheelsBuilder basic = new HotWheelsBuilder();
+        director.buildBasic(basic);
+        basic
+                .id(4)
+                .seriesName("Mainline")
+                .model("Batmobile")
+                .color("black");
+        System.out.println(basic.build());
     }
 }
--- Builder/src/director/Director.java
+++ Builder/src/director/Director.java
@@ -6,13 +6,73 @@ public class Director {
 
   public void buildAcceleCharged(HotWheelsBuilder builder) {
     builder
-        .id(1)
-        .seriesName("AcceleCharged")
-        .model("RD-02")
-        .color("Blue")
-        .rarity("Common")
+        .rarity("Exclusive")
         .type("AcceleCharged")
         .wheels(4)
         .doors(2);
+  }
+
+  public void buildTreasureHunt(HotWheelsBuilder builder) {
+    builder
+        .rarity("Super Rare")
+        .type("Treasure Hunt")
+        .wheels(4)
+        .doors(4);
+  }
+
+  public void buildZamac(HotWheelsBuilder builder) {
+    builder
+        .rarity("Exclusive")
+        .type("Zamac")
+        .wheels(4)
+        .doors(4);
+  }
+
+  public void buildBasic(HotWheelsBuilder builder) {
+    builder
+        .rarity("Common")
+        .type("Basic")
+        .wheels(4)
+        .doors(4);
+  }
+
+  public void buildChrome(HotWheelsBuilder builder) {
+    builder
+        .rarity("Rare")
+        .type("Chrome")
+        .wheels(4)
+        .doors(4);
+  }
+
+  public void buildEpic(HotWheelsBuilder builder) {
+    builder
+        .rarity("Super Rare")
+        .type("Epic Fast")
+        .wheels(4)
+        .doors(4);
+  }
+
+  public void buildFlame(HotWheelsBuilder builder) {
+    builder
+        .rarity("Ultra Rare")
+        .type("Exotic")
+        .wheels(4)
+        .doors(2);
+  }
+
+  public void buildNFT(HotWheelsBuilder builder) {
+    builder
+        .rarity("Ultra Rare")
+        .type("NFT")
+        .wheels(4)
+        .doors(2);
+  }
+
+  public void buildPremium(HotWheelsBuilder builder) {
+    builder
+        .rarity("Uncommon")
+        .type("Premium")
+        .wheels(4)
+        .doors(4);
   }
 }
