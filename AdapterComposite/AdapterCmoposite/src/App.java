ec0b0d359540b68cb0ff9328009b796071570032
Jorge Mario Lasso Ortiz <jorgelasso_1992@hotmail.com>
2024-10-16 15:44:52 -0500

    Implemented validation on App for user authentication

--- AdapterComposite/AdapterCmoposite/src/App.java
+++ AdapterComposite/AdapterCmoposite/src/App.java
@@ -1,23 +1,31 @@
 public class App {
   public static void main(String[] args) throws Exception {
+    User user = new User("John", "Doe", "johndoe", "password123");
 
-    Organization organization1 = new Organization("Org");
-    Headquarter headquarter = new Headquarter("HQ");
-    Dependency dependency1 = new Dependency("dep1");
-    Dependency dependency2 = new Dependency("dep2");
+    UserAdapter userAdapter = new UserAdapter(user);
 
-    organization1.add(headquarter);
-    headquarter.add(dependency1);
-    dependency1.add(dependency2);
-    headquarter.add(new Dependency("dep3"));
-    dependency1.add(new Dependency("dep4"));
+    AuthenticationService authenticationService = new AuthenticationService(userAdapter);
 
-    organization1.showInfo();
+    if (authenticationService.Authenticate("johndoe", "password123")) {
+      Organization organization1 = new Organization("Org");
+      Headquarter headquarter = new Headquarter("HQ");
+      Dependency dependency1 = new Dependency("dep1");
+      Dependency dependency2 = new Dependency("dep2");
 
-    headquarter.addEmployee(new Employee("Emp1"));
-    headquarter.addEmployee(new Employee("Emp2"));
+      organization1.add(headquarter);
+      headquarter.add(dependency1);
+      dependency1.add(dependency2);
+      headquarter.add(new Dependency("dep3"));
+      dependency1.add(new Dependency("dep4"));
 
-    organization1.showEmployees();
+      organization1.showInfo();
 
+      headquarter.addEmployee(new Employee("Emp1"));
+      headquarter.addEmployee(new Employee("Emp2"));
+
+      organization1.showEmployees();
+    } else {
+      System.out.println("Not Authenticated");
+    }
   }
 }
