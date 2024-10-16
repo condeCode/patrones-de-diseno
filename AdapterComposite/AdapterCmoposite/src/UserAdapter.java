ae5757de3c8e9c2f39b52bc6645ed786f29d1e6f
lizegira <lizegira@bancolombia.com.co>
2024-10-16 09:53:42 -0500

    Remove authentication interface

--- AdapterComposite/AdapterCmoposite/src/UserAdapter.java
+++ AdapterComposite/AdapterCmoposite/src/UserAdapter.java
@@ -1,4 +1,4 @@
-public class UserAdapter implements INewAuthentication {
+public class UserAdapter {
 
   private User user;
 
@@ -6,42 +6,34 @@ public class UserAdapter implements INewAuthentication {
     this.user = user;
   }
 
-  @Override
   public String getFirstName() {
     return user.getFirstName();
   }
 
-  @Override
   public String getLastName() {
     return user.getLastName();
   }
 
-  @Override
   public String getUserName() {
     return user.getUserName();
   }
 
-  @Override
   public Boolean checkUsernameAndPassword(String userName, String password) {
     return user.checkUsernameAndPassword(userName, password);
   }
 
-  @Override
   public void setFirstName(String firstName) {
     user.setFirstName(firstName);
   }
 
-  @Override
   public void setLastName(String lastName) {
     user.setLastName(lastName);
   }
 
-  @Override
   public void setUserName(String userName) {
     user.setUserName(userName);
   }
 
-  @Override
   public void setPassword(String password) {
     user.setPassword(password);
   }
